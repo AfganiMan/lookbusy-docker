5d6879422262dda53d0becddcc1f82ec38aac42d
Afgani <160530591+AfganiMan@users.noreply.github.com>
2024-03-06 02:22:57 +0700

    Update start.arm.sh

--- start.arm.sh
+++ start.arm.sh
@@ -1,2 +1,10 @@
 /app/lookbusy/lookbusy -c 50-60 -r curve -n 2
 /app/lookbusy/lookbusy -c 50-60 -r curve -n 5
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 2
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 5
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 2
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 5
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 2
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 5
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 456
+/app/lookbusy/lookbusy -c 50-60 -r curve -n 5
